a4b8473d61b610b013896d6b1cde56763efa71f6
aoles <andrzej.oles@gmail.com>
2024-04-15 17:26:25 +0200

    test: switch profile in extra info processor api test to hgv
    The test doesn't pass with the car profile because now the profile relies on country data attached to nodes in the PBF file. The original border geometries are no longer processed by ORS so countries for virtual nodes/edges cannot be resolved anymore.

--- ors-api/src/test/java/org/heigit/ors/apitests/routing/ResultTest.java
+++ ors-api/src/test/java/org/heigit/ors/apitests/routing/ResultTest.java
@@ -3089,7 +3089,7 @@ class ResultTest extends ServiceTest {
         // Close to a border crossing
         given()
                 .headers(CommonHeaders.jsonContent)
-                .pathParam("profile", getParameter("carProfile"))
+                .pathParam("profile", "driving-hgv")
                 .body(body.toString())
                 .when()
                 .post(getEndPointPath() + "/{profile}/json")
